3e3a22c01db5ac986277336ab49888d6e53884c5
Max <81183216+MoustikBe@users.noreply.github.com>
2024-05-05 23:46:53 +0200

    Add files via upload

---
+++ ppx/new_pipe/bns_ppx.h
@@ -0,0 +1,13 @@
+#ifndef BNS_PPX
+#define BNS_PPX
+// The library // 
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <strings.h>
+# include <sys/wait.h>
+# define BUFFER_SIZE 5
+// The library // 
+char	**ft_split(char const *s, char c);
+#endif
