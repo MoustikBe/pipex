44f0ef75e79539b02b1733e523d8fa9f6148aa03
Max <81183216+MoustikBe@users.noreply.github.com>
2024-05-07 23:33:59 +0200

    Add files via upload

---
+++ ppx/ppx_bonus/src_bonus/ppx_main.c
@@ -0,0 +1,33 @@
+#include "../pipex.h"
+
+void	command_execution(char **envp, char **argv, int index, char *str)
+{
+    char **split_path;
+    char **arguments;
+    char *path;
+    int id_path;
+
+    id_path = path_finder(envp);
+    split_path = ft_split(envp[id_path], ':');
+    path = locate_path(split_path, argv[index]);
+    if(path == NULL)
+        error() ;
+    arguments = ft_split(str, ' ');
+    execve(path, arguments, NULL);
+}
+
+//void createProcessus()
+//{
+//
+//}
+
+
+int main(int argc, char **argv, char **envp)
+{
+
+	return(0);
+}
+
+
+
+
